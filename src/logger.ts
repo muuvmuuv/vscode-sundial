71ee64dbcd3509ab10ebf635572689f1bb70fc02
Marvin Heilemann <marvin.heilemann@googlemail.com>
2019-10-11 11:07:37 +0200

    Removed console color output

--- src/logger.ts
+++ src/logger.ts
@@ -1,19 +1,15 @@
 import logger from 'loglevel'
 
-const color = {
-  orange: 'color: #a06a00;',
-}
-
 const originalFactory = logger.methodFactory
 
 logger.methodFactory = (methodName, logLevel, loggerName) => {
   const rawMethod = originalFactory(methodName, logLevel, loggerName)
   const devider = ':'
   const name = loggerName ? devider + loggerName : ''
-  const prefix = `%c(Sundial${name}) => `
+  const prefix = `(Sundial${name}) => `
 
   return (...messages) => {
-    rawMethod(prefix, color.orange, ...messages)
+    rawMethod(prefix, ...messages)
   }
 }
 
