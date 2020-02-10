501c64d4aab410bf184ce1746868c25938c1752b
Marvin Heilemann <marvin.heilemann@googlemail.com>
2020-02-10 21:03:25 +0100

    feat: removed tests
    they are not necessary and I don't have time to manage to get them working

--- src/test/suite/commands.spec.ts
+++
@@ -1,7 +0,0 @@
-import vscode from 'vscode'
-
-suite('Commands', () => {
-  test('Should toggle the theme', async () => {
-    await vscode.commands.executeCommand('sundial.toggleDayNightTheme')
-  })
-})
