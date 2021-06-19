7bdebf34656d445bd648549e3ae5acb9855a95ae
Marvin Heilemann <m.heilemann@protonmail.com>
2021-06-19 13:04:48 +0200

    build: update stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,54 @@
 // @ts-check
 
 const path = require('path')
-const { BannerPlugin } = require('webpack')
 
-const package_ = require('./package.json')
+/**
+ * Webpack setup.
+ *
+ * @param {object} environment - environment
+ * @param {object} argv - webpack arguments
+ *
+ * @type {import('webpack').Configuration}
+ */
+module.exports = (environment, argv) => {
+  console.log('Env:', argv.mode)
 
-const banner = `${'-'.repeat(20)}
-${package_.displayName} (${package_.name})
-${package_.description}
+  const isDevelopment = argv.mode === 'development' || false
 
-@version ${package_.version}
-@license ${package_.license}
-@author ${package_.author.name} (${package_.author.url})
-@readme ${package_.homepage}
-@pkg ${package_.repository}
-${'-'.repeat(20)}`
-
-/** @type {import('webpack').Configuration} */
-const config = {
-  target: 'node',
-  mode: 'none',
-  entry: './src/extension.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'extension.js',
-    libraryTarget: 'commonjs2',
-  },
-  devtool: 'nosources-source-map',
-  externals: {
-    // the vscode-module is created on-the-fly and must be excluded.
-    // See: https://webpack.js.org/configuration/externals/
-    vscode: 'commonjs vscode',
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  optimization: {
-    concatenateModules: true,
-    mangleExports: false,
-    minimize: process.env.NODE_ENV === 'production',
-  },
-  plugins: [new BannerPlugin(banner)],
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader',
-          },
-        ],
-      },
-    ],
-  },
+  return {
+    target: 'node',
+    mode: 'none',
+    entry: './src/extension.ts',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'extension.js',
+      libraryTarget: 'commonjs2',
+    },
+    devtool: isDevelopment ? 'nosources-source-map' : 'hidden-source-map',
+    externals: {
+      // the vscode-module is created on-the-fly and must be excluded.
+      // See: https://webpack.js.org/configuration/externals/
+      vscode: 'commonjs vscode',
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    optimization: {
+      concatenateModules: true,
+      minimize: !isDevelopment,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'ts-loader',
+            },
+          ],
+        },
+      ],
+    },
+  }
 }
-
-module.exports = config
